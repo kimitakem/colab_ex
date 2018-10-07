57d5c949b038682ed66a04e7ab783eeaef78952e
Kimitake Mizobe <kimimizobe@gmail.com>
2018-10-07 14:57:09 +0900

    Validationの追加

--- eager_iris.py
+++ eager_iris.py
@@ -13,8 +13,8 @@ iris = datasets.load_iris()
 all_x = preprocessing.scale(iris.data).astype('float32')  # データのスケーリングと標準化
 all_y = iris.target
 
-# 訓練データとテストデータに分割
-train_x, test_x, train_y, test_y = model_selection.train_test_split(all_x, all_y, train_size=0.8)
+# 訓練データと検証データに分割（8割（120サンプル）を訓練データに）
+train_x, val_x, train_y, val_y = model_selection.train_test_split(all_x, all_y, train_size=0.8)
 
 #############################################
 from tensorflow import keras
@@ -32,7 +32,7 @@ def get_optimizer():
     return tf.train.GradientDescentOptimizer(learning_rate=0.01)
 
 batch_size = 32
-num_epochs = 100
+num_epochs = 1000
 
 
 #############################################
@@ -43,14 +43,15 @@ def keras_training():
                   optimizer=optimizer,
                   metrics=['accuracy'])
 
-    hist = model.fit(train_x, train_y, epochs=num_epochs, batch_size=batch_size)
+    hist = model.fit(train_x, train_y, epochs=num_epochs, batch_size=batch_size, validation_data=(val_x, val_y))
     return hist
 
 keras_hist = keras_training()
 
 #############################################
 from tensorflow.data import Dataset
-dataset_train = Dataset.from_tensor_slices((train_x, train_y)).batch(batch_size)
+train_dataset = Dataset.from_tensor_slices((train_x, train_y)).batch(batch_size)
+val_dataset = Dataset.from_tensor_slices((val_x, val_y)).batch(batch_size)
 
 
 #############################################
@@ -78,44 +79,49 @@ def grad(model, inputs, targets):
 
 #############################################
 def eager_training():
-    eager_model = get_model_wo_softmax()
+    model = get_model_wo_softmax()
     optimizer = get_optimizer()
     global_step = tf.train.get_or_create_global_step()
 
-    # keep results for plotting
-    train_loss_results = []
-    train_accuracy_results = []
+    # 損失関数の値・精度を残しておくためのリスト
+    train_loss_list = []
+    train_accuracy_list = []
+    val_loss_list = []
+    val_accuracy_list = []
 
     for epoch in range(num_epochs):
         epoch_loss_avg = tfe.metrics.Mean()
         epoch_accuracy = tfe.metrics.Accuracy()
-        # Training loop - using batches of 32
 
-        for b, (x, y) in enumerate(dataset_train):
-            print(b, y.shape)
+        # 学習のループ
+        for b, (x, y) in enumerate(train_dataset):
             # Optimize the model
-            loss_value, grads = grad(eager_model, x, y)
-            optimizer.apply_gradients(zip(grads, eager_model.variables),
+            loss_value, grads = grad(model, x, y)
+            optimizer.apply_gradients(zip(grads, model.variables),
                                       global_step)
 
             # Track progress
             epoch_loss_avg(loss_value)  # add current batch loss
             # compare predicted label to actual label
-            epoch_accuracy(tf.argmax(eager_model(x), axis=1, output_type=tf.int32), y)
+            epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
 
-        # end epoch
-        train_loss_results.append(epoch_loss_avg.result())
-        train_accuracy_results.append(epoch_accuracy.result())
+        train_loss_list.append(epoch_loss_avg.result())
+        train_accuracy_list.append(epoch_accuracy.result())
 
-        if epoch % 5 == 0:
-            print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                        epoch_loss_avg.result(),
-                                                                        epoch_accuracy.result()))
+        # 検証のループ
+        epoch_test_loss_avg = tfe.metrics.Mean()
+        epoch_test_accuracy = tfe.metrics.Accuracy()
+        for b, (x, y) in enumerate(val_dataset):
+            epoch_test_loss_avg(loss(model, x, y))
+            epoch_test_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
 
-    return train_loss_results, train_accuracy_results
+        val_loss_list.append(epoch_test_loss_avg.result())
+        val_accuracy_list.append(epoch_test_accuracy.result())
+
+    return train_loss_list, train_accuracy_list, val_loss_list, val_accuracy_list
 
 
-train_loss_results, train_accuracy_results = eager_training()
+train_loss_results, train_accuracy_results, val_loss_results, val_accuracy_results = eager_training()
 
 ######################################
 import matplotlib.pyplot as plt
@@ -123,16 +129,23 @@ import numpy as np
 x = range(num_epochs)
 plt.plot(x, train_accuracy_results, label="eager_acc")
 plt.plot(x, keras_hist.history['acc'], label="keras_acc")
+plt.plot(x, val_accuracy_results, label="eager_val_acc")
+plt.plot(x, keras_hist.history['val_acc'], label="keras_val_acc")
 plt.title("accuracy")
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.legend(loc='bottom right')
 plt.show()
 
 plt.plot(x, train_loss_results, label="eager_loss")
 plt.plot(x, keras_hist.history['loss'], label="keras_loss")
+plt.plot(x, val_loss_results, label="eager_val_loss")
+plt.plot(x, keras_hist.history['val_loss'], label="keras_loss")
 plt.title("loss")
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.legend(loc='top right')
 plt.show()
 
+
+import ipdb
+ipdb.set_trace()
 ######################################
 for i in range(20):
     keras_hist = keras_training()
